3eb49f87cacdcd75ae96454ae24421c2aebb0e12
Naja386438 <165750458+Naja386438@users.noreply.github.com>
2024-04-17 12:27:40 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import './App.css';
+import LoginPage from './pages/loginPage';
+import RegisterPage from './pages/registerPage';
+import {store, persistor} from './redux/store';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import HomePage from './pages/home';
+import { PersistGate } from 'redux-persist/integration/react';
+import ForgetPage from './pages/Forget';
+import ResetPage from './pages/reset';
+import CraftPage from './pages/craftPage';
+
+function App() {
+  return (
+    <Provider store={store}>
+            <PersistGate loading={null} persistor={persistor}>
+
+    <Router> 
+      <Routes>
+        <Route path="/" element={<HomePage />} />  
+        <Route path="/login" element={<LoginPage />} /> 
+        <Route path="/Forget" element={<ForgetPage/>} /> 
+        <Route path="/Reset" element={<ResetPage/>} /> 
+        <Route path="/Craft" element={<CraftPage/>} /> 
+        <Route path="/register" element={<RegisterPage />} />  
+        <Route path="/*" element={<RegisterPage />} />  
+      </Routes> 
+    </Router> 
+    </PersistGate>
+
+    </Provider> 
+  );
+}
+
+export default App;
